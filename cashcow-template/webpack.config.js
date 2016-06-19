230c1d05065eefc5c05ebb76bab3775a10ffda8e
Chris Helgert <chris.helgert@yahoo.com>
2016-06-19 13:07:18 +0200

    webpack config with babel and css

---
+++ cashcow-template/webpack.config.js
@@ -0,0 +1,22 @@
+/* eslint arrow-body-style: 0 */
+const path = require('path');
+
+module.exports = env => {
+  return {
+    entry: path.resolve(__dirname, 'public/javascripts/client.js'),
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      pathinfo: !env.prod,
+    },
+    context: path.resolve(__dirname, 'public/javascripts'),
+    devtool: env.prod ? 'source-map' : 'eval',
+    bail: env.prod,
+    module: {
+      loaders: [
+        { test: /\.js(x)$/, loader: 'babel!eslint', exclude: /node_modules/ },
+        { test: /\.css$/, loader: 'style!css' },
+      ],
+    },
+  };
+};
