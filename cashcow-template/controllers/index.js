73a47b998bac5f2b79c105e8d4d9789f575503cc
Daniel Bayerlein <daniel.bayerlein@googlemail.com>
2016-06-19 10:14:16 +0200

    Remove comment

--- cashcow-template/controllers/index.js
+++ cashcow-template/controllers/index.js
@@ -1,5 +1,3 @@
-// https://github.com/semateos/stimpy-medium/blob/4166eaf261dabb60d2afe01bca2f89b489ea6e1d/server/routes/base.js
-
 module.exports = {
   index: {
     handler: (request, reply) => {
