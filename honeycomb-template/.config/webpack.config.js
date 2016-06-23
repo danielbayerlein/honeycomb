9b1e1828a70d7809f343f37eb3a664186d4cc8d2
Daniel Bayerlein <daniel.bayerlein@googlemail.com>
2016-06-23 17:23:47 +0200

    Remove absolute path from entry

--- honeycomb-template/.config/webpack.config.js
+++ honeycomb-template/.config/webpack.config.js
@@ -6,7 +6,7 @@ const Hoek = require('hoek');
 const defaultConfig = {
   entry: {
     app: [
-      path.resolve(__dirname, '../src/client/client.js'),
+      './src/client/client.js',
     ],
   },
   output: {
