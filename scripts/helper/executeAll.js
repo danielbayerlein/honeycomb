9b21b7fb71b0ed7687cde4db25b03f94e1e5fd5c
Chris Helgert <chrishelgert@users.noreply.github.com>
2016-09-29 15:23:15 +0200

    optimize execution-scripts (#31)
    * es6-generator for better output on recursive npm test
    * es6-generator for better output on recursive npm install
    * filter npm warnings and empty lines from stderr to prevent build-break
    * optimized npm-warn-filter and assignment to project

--- scripts/helper/executeAll.js
+++ scripts/helper/executeAll.js
@@ -1,49 +1,112 @@
-const proc = require('child_process');
+const spawn = require('child_process').spawn;
 const path = require('path');
 
-let errors = false;
+let error = false;
+
+const NPM_WARNINGS = [
+  'npm',
+  'deprecated',
+  'WARN',
+  'No description',
+  'No repository',
+];
 
 /**
- * Execute the given command for the given project
- * relative from the root-folder.
- * if an error occurs it set true to the global error-variable
+ * Check if the error-message is a npm-warning
+ * to prevent build-break
  *
- * @param  {string} command shell-command
- * @param  {string} project project-name
- * @return {void}
+ * @param  {string}   msg  error-message
+ * @return {boolean}       is npm warning
  */
-function executeCommand(command, project) {
-  console.log(`start ${command} for ${project}`);
+function isNPMWarning(msg) {
+  return NPM_WARNINGS.filter(el => msg.indexOf(el) !== -1).length > 0;
+}
+
+/**
+ * Creates a promise-wrapper around the child_process.spawn-API
+ * calls the command with args for the project-name
+ *
+ * @param  {string}  command shell command (example: npm)
+ * @param  {array}   args    arguments for the command (example: test)
+ * @param  {string}  project project-name (folder)
+ * @return {promise}         resolvable promise
+ */
+function spawnPromise(command, args, project) {
+  const commandForProject = `${command} ${args} for ${project}`;
+
+  console.log(`start ${commandForProject}`);
   process.chdir(path.join(__dirname, '..', '..', project));
 
-  try {
-    const result = proc.execSync(`${command}`);
-    console.log(result.toString());
-  } catch (error) {
-    console.error(`${command} for ${project} failed`);
-    errors = true;
-  }
+  // creates a promise wrapper around the stream
+  return new Promise((resolve) => {
+    let localError = false;
+    const proc = spawn(command, args);
 
-  console.log(`finished ${command} for ${project}`);
+    proc.stdout.on('data', (data) => {
+      console.log(data.toString());
+    });
+
+    proc.stderr.on('data', (data) => {
+      const dataString = data.toString();
+
+      // Filter empty stderr or npm WARN messages
+      if (dataString.trim().length && !isNPMWarning(dataString)) {
+        localError = true;
+      }
+
+      console.error(dataString);
+    });
+
+    proc.on('close', () => {
+      if (localError) {
+        console.log(`finished ${commandForProject} with errors`);
+        error = true;
+      } else {
+        console.log(`finished ${commandForProject} successfully`);
+      }
+
+      resolve();
+    });
+  });
+}
+
+/**
+ * Generator to iterate over the projects
+ *
+ * @param  {array}      projects array with all projects
+ * @return {generator}           generator with all projects
+ */
+function* genProjects(projects) {
+  yield* projects;
 }
 
 /**
  * Executes the given command for all projects
  * calls process.exit(1) if an error occurs
  *
- * @param  {string} command  shell-command
+ * @param  {string} command  shell-command (example: npm)
+ * @param  {string} args     arguments for the command (example: test)
  * @param  {array}  projects project-names
  * @return {void}
  */
-function executeAll(command, projects) {
-  projects.forEach(project => executeCommand(command, project));
+function executeAll(command, args, projects) {
+  const it = genProjects(projects);
+  let currentProject;
 
-  if (errors) {
-    console.error(`${command} failed`);
-    process.exit(1);
-  } else {
-    console.log(`${command} successful`);
-  }
+  (function iterate() {
+    currentProject = it.next();
+
+    if (currentProject.done) {
+      if (error) {
+        console.error(`${command} ${args} failed`);
+        process.exit(1);
+      } else {
+        console.log(`${command} ${args} successful`);
+      }
+    } else {
+      spawnPromise(command, args, currentProject.value).then(() => iterate());
+    }
+  }());
 }
 
 module.exports = executeAll;
--- scripts/install.js
+++ scripts/install.js
@@ -1,4 +1,4 @@
 const executeAll = require('./helper/executeAll');
 const projects = require('./helper/projects');
 
-executeAll('npm install', projects);
+executeAll('npm', ['install'], projects);
