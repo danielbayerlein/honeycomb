cf355a406017b5196a37baa02b9f98d8a658bb9a
Chris Helgert <chris.helgert@yahoo.com>
2016-10-09 16:58:34 +0200

    removing existing folders with rimraf and install test-projects manually

--- scripts/generate.js
+++ scripts/generate.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const childProcess = require('child_process');
+const rimraf = require('rimraf').sync;
 
 const variations = ['handlebars', 'react'];
 
@@ -13,15 +14,14 @@ variations.forEach(variation => {
   console.log(`create ${variation}-microservice with yo`);
   const folder = path.join(__dirname, '..', `test-${variation}`);
 
-  try {
-    fs.mkdirSync(folder);
-  } catch (error) {
-    fs.rmdirSync(folder);
-    fs.mkdirSync(folder);
-  }
+  rimraf(folder);
+  fs.mkdirSync(folder);
 
   process.chdir(folder);
   childProcess.execSync(`node ../node_modules/yo/lib/cli.js honeycomb --author test --name test-${variation} --description "testsetup for ${variation}" --template "${variation}" --skip-install`);
+
+  childProcess.execSync('npm link ../honeycomb-registry-client');
+  childProcess.execSync('npm install');
 
   console.log(`created ${variation}-microservice successfully`);
 });
