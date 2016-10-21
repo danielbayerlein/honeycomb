68e94673e826167986fc84f401eb042102b45f18
Chris Helgert <chrishelgert@users.noreply.github.com>
2016-10-21 11:09:04 +0200

    ternary instead of if/else

--- scripts/install.js
+++ scripts/install.js
@@ -2,9 +2,4 @@ const caniuseYarn = require('@danielbayerlein/caniuse-yarn');
 const executeAll = require('./helper/executeAll');
 const projects = require('./helper/projects');
 
-if (caniuseYarn()) {
-  executeAll('yarn', ['install'], projects);
-} else {
-  executeAll('npm', ['install'], projects);
-}
-
+executeAll(caniuseYarn() ? 'yarn' : 'npm', ['install'], projects);
