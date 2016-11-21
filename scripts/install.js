81142246d482bab23d7c30725cfae3433a5cad8b
Daniel Bayerlein <danielbayerlein@users.noreply.github.com>
2016-11-21 11:02:22 +0100

    Switch to Lerna (#51)
    * Add Lerna - A tool for managing JavaScript projects with multiple packages
    
    * Add clean task
    
    * Remove unused scripts
    
    * Update cache dirs
    
    * Replace ghooks with husky
    
    * Update license link
    
    * Generate examples to packages/
    
    * Run test with scope
    
    * Fix ESLint issue

--- scripts/install.js
+++
@@ -1,5 +0,0 @@
-const caniuseYarn = require('@danielbayerlein/caniuse-yarn');
-const executeAll = require('./helper/executeAll');
-const projects = require('./helper/projects');
-
-executeAll(caniuseYarn() ? 'yarn' : 'npm', ['install'], projects);
--- scripts/test.js
+++
@@ -1,4 +0,0 @@
-const executeAll = require('./helper/executeAll');
-const projects = require('./helper/projects');
-
-executeAll('npm', ['test'], projects);
