8f7db2b3a164cf338257fd1250ef8f3480785a96
Daniel Bayerlein <daniel.bayerlein@googlemail.com>
2016-12-05 19:13:18 +0100

    Add tests for honeycomb-registry-client

---
+++ packages/honeycomb-registry-client/__tests__/index-test.js
@@ -0,0 +1,46 @@
+jest.mock('eureka-js-client');
+
+const register = require('../').register;
+const Eureka = require('eureka-js-client').Eureka;
+
+describe('honeycomb-registry-client', () => {
+  let client;
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+    client = { on: jest.fn(), start: jest.fn() };
+    Eureka.mockImplementation(() => (client));
+    console.info = jest.genMockFn(); // eslint-disable-line no-console
+    register('test', '127.0.0.1', 3000);
+  });
+
+  it('should passed expected options to Eureka', () => {
+    expect(Eureka.mock.calls[0][0]).toEqual({
+      eureka: {
+        host: 'localhost',
+        port: '8761',
+        servicePath: '/eureka/apps/',
+      },
+      instance: {
+        app: 'test',
+        dataCenterInfo: {
+          '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+          name: 'MyOwn',
+        },
+        healthCheckUrl: 'http://127.0.0.1:3000/health',
+        homePageUrl: 'http://127.0.0.1:3000/',
+        hostName: '127.0.0.1',
+        port: {
+          '$': 3000, // eslint-disable-line quote-props
+          '@enabled': 'true',
+        },
+        statusPageUrl: 'http://127.0.0.1:3000/info',
+        vipAddress: '127.0.0.1',
+      },
+    });
+  });
+
+  it('should be called the start function', () => {
+    expect(client.start).toHaveBeenCalled();
+  });
+});
