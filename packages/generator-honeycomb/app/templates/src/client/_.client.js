52d0ece38ccdc610c231aca57152f058f73df8bd
Chris Helgert <chris.helgert@online.de>
2017-01-19 23:00:27 +0100

    optimized client.js, fixed error in package.json for docker-build

--- packages/generator-honeycomb/app/templates/src/client/_.client.js
+++ packages/generator-honeycomb/app/templates/src/client/_.client.js
@@ -6,26 +6,29 @@ import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import Example from './components/Example';
 
-const appEl = document.querySelector('.js-app');
-const name = 'world';
+document.addEventListener('DOMContentLoaded', () => {
+  const appEl = document.querySelector('.js-app');
+  const name = 'world';
 
-render(
-  <AppContainer>
-    <Example name={name} />
-  </AppContainer>,
-  appEl
-);
+  render(
+    <AppContainer>
+      <Example name={name} />
+    </AppContainer>,
+    appEl
+  );
 
-if (module.hot) {
-  module.hot.accept(() => {
-    render(
-      <AppContainer>
-        <Example name={name} />
-      </AppContainer>,
-      appEl
-    );
-  });
-}
+  if (module.hot) {
+    module.hot.accept(() => {
+      render(
+        <AppContainer>
+          <Example name={name} />
+        </AppContainer>,
+        appEl
+      );
+    });
+  }
+}, false);
+
 <%_ } _%>
 <%_ if (includeHandlebars) { _%>
 import './client.css';
