d3a1f08d3dd982fca47906a5a139e9cbf51ab0fc
Daniel Bayerlein <daniel.bayerlein@googlemail.com>
2017-01-21 17:21:26 +0100

    Move to honeycomb-server

--- packages/honeycomb-layout/src/index.js
+++ packages/honeycomb-layout/src/index.js
@@ -1,30 +1,16 @@
-import Hapi from 'hapi';
-import Hoek from 'hoek';
+import HoneycombServer from 'honeycomb-server';
 import Path from 'path';
-import tailor from 'hapi-tailor-middleware';
-import logging from 'honeycomb-logging-middleware';
 
-const server = new Hapi.Server();
-
-server.connection({
-  port: process.env.PORT || 3000,
-});
-
-server.register([
-  {
-    register: logging,
-  },
-  {
-    register: tailor,
-    options: {
-      templatesPath: Path.join(__dirname, '..', 'templates'),
+HoneycombServer.start({
+  plugins: [
+    {
+      module: 'honeycomb-logging-middleware',
     },
-  },
-], (registerError) => {
-  Hoek.assert(!registerError, registerError);
-
-  server.start((startError) => {
-    Hoek.assert(!startError, startError);
-    console.info('Server running at:', server.info.uri); // eslint-disable-line no-console
-  });
+    {
+      module: 'hapi-tailor-middleware',
+      options: {
+        templatesPath: Path.join(__dirname, '..', 'templates'),
+      },
+    },
+  ],
 });
