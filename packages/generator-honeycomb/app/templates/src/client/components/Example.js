1cd51c5aa622ba28d21b14ec3ccc4485270f5956
Daniel Bayerlein <daniel.bayerlein@googlemail.com>
2017-03-10 15:26:17 +0100

    Fix documentation and example

--- packages/generator-honeycomb/app/templates/src/client/components/Example.js
+++ packages/generator-honeycomb/app/templates/src/client/components/Example.js
@@ -2,13 +2,13 @@
 import { h } from 'preact';
 
 /**
- * Example for react on the client
+ * Example for preact on the client
  *
  * @param  {string}   name  display-name
- * @return {element}        react-component
+ * @return {element}        preact-component
  */
 const Example = ({ name }) => (
-  <h2>Hi {name}, this is an example for rendering React on the client.</h2>
+  <h2>Hi {name}, this is an example for rendering Preact on the client.</h2>
 );
 
 export default Example;
